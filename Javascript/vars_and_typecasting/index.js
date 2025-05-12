0cff3a7f1ceedacb1f90255ccaf704795e01606e
Mirza Asfandyar Baig <mirza.business15@gmail.com>
2025-05-12 23:57:47 +0500

    Variables in JS

---
+++ Javascript/vars_and_typecasting/index.js
@@ -0,0 +1,52 @@
+// variables below!!
+
+// let x = 100;
+// console.log(x);
+// console.log(typeof x);
+// console.log(`You are $(x) years old`);
+// console.log("Your price is $$(x)");
+
+// let firstName = "Mirza"
+// let favoriteFood = "Pizza"
+// let email = "hello123@gmail.com"
+
+// let online = false;
+// let forSale = true;
+// let isStudent = true;
+
+// console.log(`Bro's online: $(online)`);
+// console.log(`Bro's for sale: $(forSale)`);
+// console.log("Enrolled: $(isStudent)");
+
+
+// type conversions!!
+
+// let age = window.prompt("How old are you? ");
+// age = Number(age); // now it works normally
+// age+=1; //appends 1 to the input instead of adding 1 to the number
+// console.log(age, typeof age);
+
+// let x = "pizza"; //we replace pizza with 0 and empty strings.
+// let y = "pizza";
+// let z = "pizza";
+
+// x = Number(x);
+// y = String(y);
+// z = Boolean(z);
+
+// console.log(x, typeof x);
+// console.log(y, typeof y);
+// console.log(z, typeof z);
+
+// constants vs let discussed!!
+
+// const PI = 3.14159;
+// let radius;
+// let circum;
+
+// document.getElementById("mySubmit").onclick = function () {
+//     radius = document.getElementById("myText").value;
+//     radius = Number(radius);
+//     circum = 2 * PI * radius;
+//     document.getElementById("myH3").textContent = circum + "cm";
+// }
