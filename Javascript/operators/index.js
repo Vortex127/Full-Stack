38789fffffafc2a5be832f292d84b17f482f5533
Mirza Asfandyar Baig <mirza.business15@gmail.com>
2025-05-13 22:44:19 +0500

    Practiced a lil bit of operators

---
+++ Javascript/operators/index.js
@@ -0,0 +1,18 @@
+// exercise to practice logical operators
+
+let firstFavNumb = 4;
+let secondFavNumb = 7;
+
+console.log(firstFavNumb > secondFavNumb);
+console.log(firstFavNumb < secondFavNumb);
+console.log(firstFavNumb >= secondFavNumb);
+console.log(firstFavNumb <= secondFavNumb);
+console.log(firstFavNumb === secondFavNumb); // strict equality
+console.log(firstFavNumb == secondFavNumb); // loose equality
+console.log(firstFavNumb !== secondFavNumb); // strict inequality
+console.log(firstFavNumb != secondFavNumb); // loose inequality
+
+
+
+
+
